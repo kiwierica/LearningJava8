bcad8b8ceb818293104bc3288cb2c02beefd169d
ying.wu <ying.wu@antfin.com>
2019-04-14 21:34:35 +0800

    github connection test

---
+++ src/lambda/FileReader/FileReaderExample.java
@@ -0,0 +1,14 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package lambda.FileReader;
+
+/**
+*
+ * @author ying.wu 梵境
+ * @version $Id: FileReaderExample, v0.1 2019年04月14日 21:30 ying.wu Exp $
+ */
+public class FileReaderExample {
+
+}
