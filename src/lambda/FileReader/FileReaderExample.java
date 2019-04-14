24f466a8fd8e701603543aa58109f73ea36ff857
ying.wu <ying.wu@antfin.com>
2019-04-14 21:48:37 +0800

    commit FileReaderExample to demonstrate lambda expression as a parameter.

--- src/lambda/FileReader/FileReaderExample.java
+++ src/lambda/FileReader/FileReaderExample.java
@@ -4,11 +4,28 @@
  */
 package lambda.FileReader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
 *
  * @author ying.wu 梵境
  * @version $Id: FileReaderExample, v0.1 2019年04月14日 21:30 ying.wu Exp $
  */
 public class FileReaderExample {
+    public static String processFile(BufferedReaderProcessor p) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(("test.txt")))) {
+            return p.process(br);
+        }
+    }
 
+    public static void main(String[] args) {
+        try {
+            String oneLine = FileReaderExample.processFile(br -> br.readLine());
+            String twoLines = FileReaderExample.processFile(br -> br.readLine() + br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
